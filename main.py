72e77763ce552d59c935ded3caa2d0c9b36042e2
Camel KEBIR <74962114+CamelKEBIR@users.noreply.github.com>
2022-03-01 12:23:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import mplfinance as mpf
 import datetime as dt
 
 
-crypto = "ETH"
+crypto = "devise souhaité"
 currency = "EUR"
 
  
 start = dt.datetime(2021,1,1)
 end = dt.datetime.now()
 
-ETH = web.DataReader(f"{crypto}-{currency}", "yahoo" , start, end)  # Other sources, like yahoo and Alpha Vantage, do function.
+devise souhaité = web.DataReader(f"{crypto}-{currency}", "yahoo" , start, end)  # Autres sources, comme Yahoo ou Alpha Vantage.
 
 
 #print(data)
@@ -24,4 +24,4 @@ ETH = web.DataReader(f"{crypto}-{currency}", "yahoo" , start, end)  # Other sour
 #plt.show()
 # ou 
 
-mpf.plot(ETH, type="candle", volume=True, style="yahoo")
+mpf.plot(devise souhaité, type="candle", volume=True, style="yahoo")
